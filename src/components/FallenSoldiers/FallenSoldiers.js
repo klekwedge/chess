b76a3361e985d2cae3f113ed2507a97eec585e72
Andrey <antonov.andrey.10101@gmail.com>
2022-11-15 16:37:45 +0300

    refactor: rewrite the FallenSoldiers

--- src/components/FallenSoldiers/FallenSoldiers.js
+++ src/components/FallenSoldiers/FallenSoldiers.js
@@ -1,26 +1,19 @@
-import React from "react";
 import "../../index.css";
 import Square from "../Square/Square";
 
-export default class FallenSoldierBlock extends React.Component {
-  renderSquare(square, i, squareShade) {
+export default function FallenSoldierBlock(props) {
+  const renderSquare = (square, i, squareShade) => {
     return <Square piece={square} style={square.style} />;
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.props.whiteFallenSoldiers.map((ws, index) =>
-            this.renderSquare(ws, index)
-          )}
-        </div>
-        <div className="board-row">
-          {this.props.blackFallenSoldiers.map((bs, index) =>
-            this.renderSquare(bs, index)
-          )}
-        </div>
+  return (
+    <div>
+      <div className="board-row">
+        {props.whiteFallenSoldiers.map((ws, index) => renderSquare(ws, index))}
       </div>
-    );
-  }
+      <div className="board-row">
+        {props.blackFallenSoldiers.map((bs, index) => renderSquare(bs, index))}
+      </div>
+    </div>
+  );
 }
