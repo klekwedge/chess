31b891d3b2511ad73422bbbf6502a7dc34417105
Andrey <antonov.andrey.10101@gmail.com>
2022-11-15 19:52:02 +0300

    feat: add types for queen figure

--- src/pieces/queen.ts
+++ src/pieces/queen.ts
@@ -1,7 +1,7 @@
 import Piece from "./figure";
 
 export default class Queen extends Piece {
-  constructor(player) {
+  constructor(player: number) {
     super(
       player,
       player === 1
@@ -10,7 +10,7 @@ export default class Queen extends Piece {
     );
   }
 
-  isMovePossible(src, dest) {
+  isMovePossible(src: number, dest: number) {
     let mod = src % 8;
     let diff = 8 - mod;
 
@@ -22,7 +22,7 @@ export default class Queen extends Piece {
     );
   }
 
-  getSrcToDestPath(src, dest) {
+  getSrcToDestPath(src: number, dest: number) {
     let path = [],
       pathStart,
       pathEnd,
